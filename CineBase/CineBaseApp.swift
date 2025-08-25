a182022b143ea037036adc9f02791cbb498ca54e
Maciej Sołoducha <biuro@goldenapp.pl>
2025-08-25 12:49:30 +0200

    Initial Commit

---
+++ CineBase/CineBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineBaseApp.swift
+//  CineBase
+//
+//  Created by Maciej Sołoducha on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct CineBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
